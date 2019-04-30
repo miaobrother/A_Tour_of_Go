5474b70de19a85b19d79d8ebc887f851b6bc8d37
yalei.zhang <zihao121908@gmail.com>
2019-04-30 13:50:12 +0800

    类型转换

---
+++ day_Three/type-conversions.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x, y = 3, 4
+	var f = math.Sqrt(float64(x*x + y*y))
+	var z = uint(f)
+
+	fmt.Println(x, y, z)
+	fmt.Printf("The f type is: %T\n", z)
+}
