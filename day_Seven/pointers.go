aed7c3e112fcbe5ec530970033ffcfb5bf0c0dfa
Mrs.XiaoLeiZi <zihao121908@gmail.com>
2019-05-07 10:02:00 +0800

    指针保存了值的内存地址

---
+++ day_Seven/pointers.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	i, j := 42, 2701
+	p := &i // 指向 i
+	fmt.Println("The j is:", &j)
+	fmt.Printf("The p is: %v\n", *p) // 通过指针读取  i 的 值
+	 *p = 21   // 通过指针设置 i 的值
+	 fmt.Println(i)
+	 p = &j
+	 fmt.Println(*p)
+	 *p = *p /37
+	 fmt.Println(j)
+
+}
