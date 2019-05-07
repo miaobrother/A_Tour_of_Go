62fe82bc2467024c3e6431e32984973c925bd1d7
Mrs.XiaoLeiZi <zihao121908@gmail.com>
2019-05-07 12:55:54 +0800

    结构体的指针

---
+++ day_Seven/literals.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Weight float64
+	Height int
+}
+
+var (
+	v1 = Vertex{125.1, 168}
+	v2 = Vertex{Weight:126.1}
+	v3 = Vertex{}
+	p  = &Vertex{127.3, 170} // 创建一个  *Vertex 类型的结构体（指针）
+)
+
+func main() {
+	fmt.Println(v1, v2, v3, p)
+
+}
